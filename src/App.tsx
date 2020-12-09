5dd3b18f1d579e57fd1c15e194e8a057cde85743
Aude Faucheux <aude53@gmail.com>
2020-12-09 18:30:39 +0000

    move createUser helper in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { createUser } from "./Adapters/APIs";
 import GameScreen from "./Components/Game/GameScreen";
 import Login from "./Components/Login/Login";
 import ErrorMessage from "./Components/Layout/ErrorMessage";
@@ -20,6 +19,24 @@ const SlotMachineApp = () => {
   const [user, setUser] = useState<User | undefined>();
   const [errorMessage, setErrorMessage] = useState<string | undefined>();
 
+  const getUsers = async () => {
+    try {
+      const response = await axios.get(`${getServerUrl()}/users`);
+      setUsers(response.data.data);
+    } catch (e) {
+      setErrorMessage(e.message);
+    }
+  };
+
+  const createUser = async (user: NewUser): Promise<User | void> => {
+    try {
+      const response = await axios.post(`${getServerUrl()}/users`, user);
+      setUser(response.data.data);
+    } catch (e) {
+      setErrorMessage(e.message);
+    }
+  };
+
   const findUser = (usernameInput: string) => {
     const existingUser = users.find(
       ({ username }) => username === usernameInput
@@ -30,15 +47,6 @@ const SlotMachineApp = () => {
     } else {
       const newUser: NewUser = { username: usernameInput };
       createUser(newUser);
-    }
-  };
-
-  const getUsers = async () => {
-    try {
-      const res = await axios.get(`${getServerUrl()}/users`);
-      setUsers(res.data.data);
-    } catch (e) {
-      setErrorMessage(e.message);
     }
   };
 
